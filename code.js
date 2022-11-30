fa4edca47d8f8d6d1c3f35bdb42ed491e4d34ece
Colloquial <47216715+JCGithu@users.noreply.github.com>
2022-11-30 14:20:09 +0000

    truncated http return

--- code.js
+++ code.js
@@ -194,8 +194,5 @@ function UrlExists(url) {
   var http = new XMLHttpRequest();
   http.open('HEAD', url, false);
   http.send();
-  if (http.status != 404)
-    return true;
-  else
-    return false;
+  return (http.status != 404);
 }
